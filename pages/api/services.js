e58c9552383869ab29bd7b6782ecf6b14dd726ab
Twocram <2002maslovartem@gmail.com>
2022-08-15 22:18:24 +0500

    created services array

---
+++ pages/api/services.js
@@ -0,0 +1,39 @@
+export const services = [
+  {
+    type: 'Установка лицензионной Windows (ОС Windows в стоимость не входит)',
+    options: [
+      {
+        id: 1,
+        name: 'Выезд мастера для установки Windows (ОС Windows в стоимость не входит) ',
+        prices: {
+          price: 600,
+          currency: 'руб',
+        },
+        selected: false,
+      },
+      {
+        id: 2,
+        name: 'Выезд мастера для установки Windows (ОС Windows в стоимость не входит) ',
+        prices: {
+          price: 600,
+          currency: 'руб',
+        },
+        selected: false,
+      },
+    ],
+  },
+  {
+    type: 'Windows 10 на выбор',
+    options: [
+      {
+        id: 4,
+        name: 'Операционная система Windows 10 Домашняя 32/64-bit на 1ПК (ESD - электронная лицензия в конверте, все языки) (KW9-00265)',
+        prices: {
+          price: 600,
+          currency: 'руб',
+        },
+        selected: false,
+      },
+    ],
+  },
+];
